93b5731cba2c17012e54820cf6b739f33ec832b3
Aymeric <aymericponjee@gmail.com>
2020-03-28 14:08:44 +0100

    ajout donnee

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Respnse;
+use Symfony\Component\HttpFoundation\Response;
 use Doctrine\Common\Persistence\ObjectManager;
 
 use App\Entity\Evenement;
