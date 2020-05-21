60ecb9cf56b1a56c9431f57a4d0d61d616824868
Aymeric Ponjée <aymericponjee@gmail.com>
2020-05-21 15:41:58 +0200

    Update QuizzDB.js

--- assets/js/components/QuizzDB.js
+++ assets/js/components/QuizzDB.js
@@ -7,33 +7,32 @@ class QuizzDB extends React.Component {
         super(props); 
     
         this.state = {
-          data: [],
+            data: [],
         }
     }
     
-    componentDidMount() {
+      componentDidMount() {
         axios.get(`/api/quizz/`)
             .then(res => {
                 const data = res.data;
                 this.setState({ data });
-        })
-    }
+            })
+        }
     
     render() {
         return (
-            <div className="row justify-content-md-center">
-                <div className="App cadreSombre">
-                    { this.state.data.map(({ id, question, reponse1, reponse2, reponse3}) => (
-                        <div key={getRandom(id)}>{question} <br/> {reponse1}<br/>{reponse2}<br/>{reponse3}<br/></div>
-                    ))}
+            <div className="container center">
+                <div className="row justify-content-md-center">
+                    <div className="App cadreSombre">
+                        { this.state.data.map(({ id, question, reponse1, reponse2, reponse3}) => (
+                            <div key={id}>{question} <br/> {reponse1}<br/>{reponse2}<br/>{reponse3}<br/></div>
+                        ))}
+                    </div>
                 </div>
             </div>
         )
     }
 }
 
-function getRandom(id) {
-    return Math.random(id);
-}
 
 export default QuizzDB;
