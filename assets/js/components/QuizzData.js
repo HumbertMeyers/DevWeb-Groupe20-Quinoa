b5be12f2921f24fb220ad5883bbdf0948f92aa12
Aymeric Ponjée <aymericponjee@gmail.com>
2020-05-21 18:37:45 +0200

    Update QuizzData.js

--- assets/js/components/QuizzData.js
+++ assets/js/components/QuizzData.js
@@ -1,6 +1,6 @@
 export const quizzdata = [
   {
-    id: "",
+    id: this.state.id,
     question: this.state.question,
     options: [this.state.reponse1, this.state.reponse2, this.state.reponse3],
     answer: this.state.reponse1,
