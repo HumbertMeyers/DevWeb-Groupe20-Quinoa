a363d13572b11331b969963653f399ce630978b2
sebfoothock <s.raemdonck@students.ephec.be>
2020-05-22 12:34:03 +0200

    delete quizzdata

--- assets/js/components/QuizzData.js
+++ assets/js/components/QuizzData.js
@@ -1,8 +1,8 @@
-export const quizzdata = [
-  {
-    id: this.state.id,
-    question: this.state.question,
-    options: [this.state.reponse1, this.state.reponse2, this.state.reponse3],
-    answer: this.state.reponse1,
-  },
-];
+// export const quizzdata = [
+//   {
+//     id: this.state.id,
+//     question: this.state.question,
+//     options: [this.state.reponse1, this.state.reponse2, this.state.reponse3],
+//     answer: this.state.reponse1,
+//   },
+// ];
