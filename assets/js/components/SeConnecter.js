008695e876ac6ab5aa2236b69717c1d8dfcefc40
sebfoothock <s.raemdonck@students.ephec.be>
2020-05-26 10:30:36 +0200

    caché élément

--- assets/js/components/SeConnecter.js
+++ assets/js/components/SeConnecter.js
@@ -1,29 +1,40 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import { Button, FormGroup, Input } from "reactstrap";
+import { Alert } from "reactstrap";
 
-class SeConnecter extends Component {
-  render() {
-    return (
-      <div className="container center">
-        <div className="row justify-content-md-center">
-          <form className="cadreSombre">
-            <h1 className="formTitle text-center">Se Connecter</h1>
-            <FormGroup>
-              <h3>Adresse Mail</h3>
-              <Input type="email" name="email" id="email" placeholder="" />
-            </FormGroup>
-            {/* <FormGroup>
+const SeConnecter = () => {
+  const [erreur, setErreur] = useState(true); //si true disparait
+
+  const login = (event) => {
+    //change l'état de erreur
+    setErreur(event.target.erreur);
+  };
+
+  return (
+    <div className="container center">
+      <div className="row justify-content-md-center">
+        <form className="cadreSombre">
+          <h1 className="formTitle text-center">Se Connecter</h1>
+          <FormGroup>
+            <h3>Adresse Mail</h3>
+            <Input type="email" name="email" id="email" placeholder="" />
+          </FormGroup>
+          {/* <FormGroup>
               <h3>Adresse mail</h3>
               <Input type="email" name="email" id="Email" placeholder="" />
             </FormGroup> */}
-            <FormGroup className="wrapper">
-              <Button className="btnLogin">Se Connecter</Button>
-            </FormGroup>
-          </form>
-        </div>
+          <FormGroup className="wrapper">
+            <Button className="btnLogin" onClick={login}>
+              Se Connect
+            </Button>
+          </FormGroup>
+        </form>
+        <Alert color="warning" style={erreur ? { display: "none" } : {}}>
+          Nous ne connaissons pas ce mail !
+        </Alert>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default SeConnecter;
